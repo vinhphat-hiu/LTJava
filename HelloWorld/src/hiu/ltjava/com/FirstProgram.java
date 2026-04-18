771a2d92b0882a161e6c1df48c66f76ffd670954
vinhphat89 <vinhphat89@gmail.com>
2026-04-18 09:59:40 +0700

    Upload FirstProgram trong buổi 2

---
+++ HelloWorld/src/hiu/ltjava/com/FirstProgram.java
@@ -0,0 +1,10 @@
+package hiu.ltjava.com;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào các bạn!");
+	}
+
+}
---
+++ HelloWorld/src/hiu/ltjava/com/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package hiu.ltjava.com;
